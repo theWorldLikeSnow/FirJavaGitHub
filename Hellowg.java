545ede3123af97b83997f915f9e47ca03f9bb3b2
theWorldLikeSnow <58248914+theWorldLikeSnow@users.noreply.github.com>
2019-11-27 16:08:14 +0800

    Add files via upload

---
+++ Hellowg.java
@@ -0,0 +1,11 @@
+package xml;
+
+public class Hellowg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world!!");
+		System.out.println("Hellow GitHub!");
+	}
+
+}

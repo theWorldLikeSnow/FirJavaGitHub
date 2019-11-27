57101ab48233f11d0c6deba52e49bb5e1a7e96d0
theWorldLikeSnow <58248914+theWorldLikeSnow@users.noreply.github.com>
2019-11-27 16:11:59 +0800

    上传测试
    添加了输出字符串Again

--- Hellowg.java
+++ Hellowg.java
@@ -5,7 +5,7 @@ public class Hellowg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world!!");
-		System.out.println("Hellow GitHub!");
+		System.out.println("Hellow GitHub!Again.");
 	}
 
 }
